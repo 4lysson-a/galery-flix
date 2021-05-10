8f605135a583b53c2c5678a2d224e8a14698642c
4ly-a <4ly.alcantara@gmail.com>
2021-05-10 02:08:00 -0300

    add jquery to do slider

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+document.write(
+  '<script type="text/javascript" src="./js/owl/setup.js"></script>',
+  '<script type="text/javascript" src="./js/owl/jquery.min.js"></script>',
+  '<script type="text/javascript" src="./js/owl/owl.carousel.min.js"></script>',
+  '<script src="https://kit.fontawesome.com/495a63b11e.js" crossorigin="anonymous"></script>'
+);
 
-Math.random().toString(36).substring(2, 15) +
-  Math.random().toString(36).substring(2, 15);
