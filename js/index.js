20326598ebb36488913e11f7030c11e7a1187697
4ly-a <4ly.alcantara@gmail.com>
2021-05-18 11:19:08 -0300

    galery-flix

--- js/index.js
+++ js/index.js
@@ -7,31 +7,36 @@ document.write(
 );
 
 var filmes = document.getElementById("film-container");
+var row = document.getElementById("carrocel");
 
 let i = 0;
+let j = 0;
 
-while (i < 50) {
-  filmes.innerHTML += `
-  <div class="item">
+while (j < 5) {
+  while (i < 50) {
+    filmes.innerHTML += `
+    <div class="item">
     <div class="info-container">
-      <div class="info-sub-container">
-        <h1 class="info-title">Como as imagens são geradas ?</h1>
-        <h2 class="info-description">
-          Os filmes são inseridos via JavaScript usando um while loop e a api do unsplash, para que as imagens inseridas sejam diferentes uma das outras foi necessário alterar as proporções e adicoinar +1 a cada elemento pelo loop
-        </h2>
-      </div>
-      <button id="film-bottom" class="botao">
-        <i class="fas fa-play-circle"></i>
-        Assistir
-      </button>
+    <div class="info-sub-container">
+    <h1 class="info-title">Como as imagens são geradas ?</h1>
+    <h2 class="info-description">
+    Os filmes são inseridos via JavaScript usando um while loop e a api do unsplash, para que as imagens inseridas sejam diferentes uma das outras foi necessário alterar as proporções e adicoinar +1 a cada elemento pelo loop
+    </h2>
+    </div>
+    <button id="film-bottom" class="botao">
+    <i class="fas fa-play-circle"></i>
+    Assistir
+    </button>
     </div>
     <img
     class="box-film"
     src="https://source.unsplash.com/${720}x${1080 + i}/"
     alt="item"
-  />
-  </div>
-`;
+    />
+    </div>
+    `;
 
-  i++;
+    i++;
+  }
+  j++;
 }
